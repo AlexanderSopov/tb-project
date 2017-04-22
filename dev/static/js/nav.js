1471d8e3367d5c9c3883d4474de53da6d50f58d3
Alexander Sopov <sopovalexander@gmail.com>
2017-04-22 14:38:20 +0200

    some fixes to hamburger

--- dev/static/js/nav.js
+++ dev/static/js/nav.js
@@ -2,10 +2,9 @@ module.exports = (function(){
 	var menu = $("header div.menu");;
 
 	function showMenu(logo){
-		if($(window).width() < 750){
-			menu.toggleClass("visible");
-			$(logo).toggleClass('open');
-		}
+		console.log("pressed");
+		menu.toggleClass("visible");
+		$(logo).toggleClass('open');
 	}
 	return {
 		showMenu:showMenu
